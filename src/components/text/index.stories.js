7ee6033b0c34a743dfd88c5a0235bc2c0cc557a6
ByeBye-Sama <joaquin.byebye@gmail.com>
2020-02-17 21:37:03 -0500

    fix: rename storybook value

--- src/components/text/index.stories.js
+++ src/components/text/index.stories.js
@@ -7,7 +7,7 @@ const metadata = {
 }
 
 export const Normal = () => {
-  const value = text('value', 'Kay Pacha')
+  const value = text('value', 'Welcome')
 
   const textStyle = text('textStyle', 'h1')
 
