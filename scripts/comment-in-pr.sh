6bf5ada6f1ae7db68342b3105b264fe665e6b991
ByeBye-Sama <joaquin.byebye@gmail.com>
2020-02-19 16:49:02 -0500

    fix text

--- scripts/comment-in-pr.sh
+++ scripts/comment-in-pr.sh
@@ -5,5 +5,5 @@ PROJECT_NAME=${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}
 GITHUB_ISSUES_URL=https://api.github.com/repos/${PROJECT_NAME}/issues
 
 curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
- -X POST -d '{"body":"🚀 Good job! The Storybook deploy is ready on ['${CIRCLE_BRANCH}']('${SURGE_URL}') 🚀"}' \
+ -X POST -d '{"body":"🚀 🎉 Storybook for ['${CIRCLE_BRANCH}']('${SURGE_URL}') 🚀"}' \
  "${GITHUB_ISSUES_URL}/${CIRCLE_PR_NUMBER}/comments"
